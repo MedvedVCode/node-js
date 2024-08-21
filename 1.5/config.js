429b92f231ffe082ee5b548de4200881029b4b6d
Fedorov Denis <ssboshen@yandex.ru>
2024-08-21 21:57:54 +0300

    create console app for city weather

---
+++ 1.5/config.js
@@ -0,0 +1,6 @@
+const WEATHER_URL = 'http://api.weatherstack.com/current';
+const API_KEY = process.env.apiKey
+
+module.exports = {
+	WEATHER_URL, API_KEY
+}
--- console.js
+++
@@ -1,9 +0,0 @@
-const { showTask:task } = require('add_seminar_task');
-task(1);
-let sum = 0;
-console.time('timer');
-for (let i = 0; i < 1000000; i++) {
-	sum += i;
-}
-console.log('sum:', sum);
-console.timeEnd('timer');
