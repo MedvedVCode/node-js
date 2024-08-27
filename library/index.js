2c89409e5f0490d3ef9c20293c499fed24eeafd2
Fedorov Denis <ssboshen@yandex.ru>
2024-08-27 10:04:28 +0300

    add ejs skeleton

--- library/index.js
+++ library/index.js
@@ -4,16 +4,21 @@ const logger = require('./middleware/logger');
 const error404 = require('./middleware/error404');
 const apiRouter = require('./routers/api');
 const userRouter = require('./routers/user');
+const ejs = require('ejs');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use( express.static( "public" ) );
 app.use(express.json());
+
 app.use(logger);
 app.use('/api', apiRouter);
 app.use('/user', userRouter);
 
-app.use(error404)
+app.use(error404);
 
 app.listen(port, () => {
 	console.log('Сервер запущен на порту', port);
